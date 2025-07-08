fa22fcd2cc927fb604f36a4fd98129cbe85c7feb
Axelius55 <axel.alacran@gmail.com>
2025-07-07 23:45:57 -0600

    se agregaron variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BreedsModule } from './breeds/breeds.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'node:process';
 
 @Module({
   imports: [
     CatsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3307,
-      username: 'user_crud',
-      database: 'db_crud',
-      password: 'root',
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT ?? '5436'),
+      username: process.env.POSTGRES_USERNAME,
+      database: process.env.POSTGRES_DATABASE,
+      password: process.env.POSTGRES_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
     }),
